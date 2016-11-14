2ea268b90f8458f0e84cd1cdbffa734bd16046c3
Hansa Srinivasan <hansa@mit.edu>
2016-11-14 12:36:47 -0500

    Added fixed gold annotation files and created token tag files

--- Sandbox.py
+++ Sandbox.py
@@ -1,33 +1,38 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[70]:
 
 import os
 
 
 # In[45]:
 
-abstract = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/20957980.txt'
+abstract = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/23127528.txt'
+abstract = 'PICO-annotations/batch5k/017e0bd245aa46b0bf1737ba34a30b2e/2648816.txt'
 abs_file = open(abstract, 'r');
 text = abs_file.read()
 
-ann = [24, 65, 322, 375, 470 ,517,529, 695, 697, 808, 900, 1020, 1330, 1410 ]
+# ann = [24, 65, 322, 375, 470 ,517,529, 695, 697, 808, 900, 1020, 1330, 1410 ]
+
+ann = [98, 160]
+# ann = [337, 432, 550, 579, 689, 789]
 
 
-# In[63]:
+# In[46]:
 
 text
 
 
 # In[47]:
 
-text_clean = text_clean.replace('\n', '')
+text_clean = text.replace('\n', '')
 text_clean = text_clean.replace(' ', '')
+text_clean = text_clean.replace('"', '``')
 text_clean
 
 
-# In[56]:
+# In[48]:
 
 new_index = 0
 
@@ -48,28 +53,17 @@ for c in range(len(text)):
     new_index += 1
 
 
-# In[57]:
+# In[15]:
 
 ann
 
 
-# In[58]:
+# In[16]:
 
 new_anns
 
 
-# In[64]:
-
-for i in range(0, len(ann), 2):
-    old = text[ann[i]:ann[i+1]]
-    new = text_clean[new_anns[i]:new_anns[i+1]]
-    print "original phrase length: ", len(old), ";  new length: ", len(new)
-    print old
-    print new
-    print " "
-
-
-# In[65]:
+# In[11]:
 
 new_index = 0
 
@@ -84,12 +78,12 @@ for a in ann:
     print a
     white = text[0:a].count(' ')
     white += text[0:a].count('\n')
-    white += text[0:a].count('"')
+    white -= text[0:a].count('"')
     new_anns.append(a-white)
 
 
 
-# In[66]:
+# In[12]:
 
 for i in range(0, len(ann), 2):
     old = text[ann[i]:ann[i+1]]
@@ -100,23 +94,144 @@ for i in range(0, len(ann), 2):
     print " "
 
 
-# In[73]:
+# In[20]:
 
 a = '"Hello my friend" he said'
 b = " ``Hello my friend'' he said"
 
 
-# In[74]:
+# In[21]:
 
 print a
 
 
-# In[75]:
+# In[22]:
 
 print b
 
 
-# In[ ]:
+# In[23]:
 
 " to ``    " ''
+
+
+# In[63]:
+
+# DEBUG VERSION
+# fixes gold annotations to not index based on white space
+def fix_gold_annotations_debug(abstract_path, gold_annotation_path):
+    abs_file = open(abstract_path, 'r');
+    text = abs_file.read()
+    
+    ann_file = open(gold_annotation_path, 'r');
+    anns = ann_file.read()
+    anns = anns.strip().split(' ')[1:]
+    
+    print anns
+    
+    anns = [int(x) for x in anns]
+    print anns
+    
+    clean_path = abstract_path[:-4] + '_tokens.txt'
+    clean_file = open(clean_path, 'r')
+    clean_text = clean_file.read().replace(' ', '')
+    
+    print text
+    print ' '
+    print clean_text
+    
+    new_anns = []
+    
+    for ann_index in anns: 
+        white = text[0:ann_index].count(' ')
+        white += text[0:ann_index].count('\n')
+        white -= text[0:ann_index].count('"')
+        new_anns.append(ann_index-white)
+    print ' '
+    
+    print anns
+    print new_anns
+    
+    for i in range(0, len(anns), 2):
+        old = text[anns[i]:anns[i+1]]
+        new = clean_text[new_anns[i]:new_anns[i+1]]
+        print "original phrase length: ", len(old), ";  new length: ", len(new)
+        print old
+        print new
+        print " "
+
+
+# In[72]:
+
+abstract = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/9665186.txt'
+annpath = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/9665186_gold.ann'
+
+abstract = 'PICO-annotations/batch5k/017e0bd245aa46b0bf1737ba34a30b2e/2648816.txt'
+annpath ='PICO-annotations/batch5k/017e0bd245aa46b0bf1737ba34a30b2e/2648816_gold.ann'
+
+fix_gold_annotations_debug(abstract, annpath)
+
+
+# In[67]:
+
+# fixes gold annotations to not index based on white space
+def fix_gold_annotations(abstract_path, gold_annotation_path):
+    abs_file = open(abstract_path, 'r');
+    text = abs_file.read()
+    
+    ann_file = open(gold_annotation_path, 'r');
+    anns = ann_file.read()
+    anns = anns.strip().split(' ')[1:]
+        
+    anns = [int(x) for x in anns]
+    
+    new_anns = []
+    
+    for ann_index in anns: 
+        white = text[0:ann_index].count(' ')
+        white += text[0:ann_index].count('\n')
+        white -= text[0:ann_index].count('"')
+        new_anns.append(ann_index-white)
+
+    new_ann_path = gold_annotation_path[:-4] + '_2.ann'
+    new_ann_file = open(new_ann_path, 'w');
+    new_anns_str = [str(x) for x in new_anns]
+    out = 'Participants ' + ' '.join(new_anns_str)
+    new_ann_file.write(out)
+
+
+# In[68]:
+
+
+abstract ='PICO-annotations/batch5k/287c157f63e44612bd3f036004df2111/22727707.txt'
+annpath ='PICO-annotations/batch5k/287c157f63e44612bd3f036004df2111/22727707_gold.ann'
+
+fix_gold_annotations(abstract, annpath)
+
+
+# In[71]:
+
+directory = 'PICO-annotations/batch5k'
+
+# For each subdirectory
+for subdir in os.listdir(directory):
+    subdir_path = directory + '/' + subdir
+    # print subdir_path
+    
+    # Not a directory
+    if not os.path.isdir(subdir_path):
+        continue
+    
+    # For each abstract in subdirectory
+    for abstract in os.listdir(subdir_path):
+        if (abstract.endswith('.txt')) and not (abstract.endswith('tokens.txt')):
+            abstract_path = subdir_path + '/' + abstract; 
+            # print abstract_path
+            ann_path = abstract_path[0:-4] + '_gold.ann'
+            fix_gold_annotations(abstract_path, ann_path)
+
+
+# In[ ]:
+
+
 
--- preprocess_data.py
+++ preprocess_data.py
@@ -1,27 +1,26 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[3]:
 
 import tensorflow as tf
 import numpy as np
 import os
 
 
-# In[42]:
+# In[4]:
 
 Null_TAG = 'None'
 P_TAG_b = 'Pb'  # beginning of participant phrase
 P_TAG_m = 'Pm'  # middle/end of participant phrase
 
 
-# In[43]:
+# In[18]:
 
-# output 2 files, .txt and .ann. 
-# .txt file has abstract, 1 sentence per line 
-# .ann has tags, 1 sentence's tag per line 
+# output 1 file .ann. 
+# .ann has tags 
+# takes token.txt file and gold_2.ann file
 def annotate_abstract(abstract_path, gold_annotation_path):
-    
     # read files 
     abs_file = open(abstract_path, 'r');
     file_text = abs_file.read();
@@ -47,7 +46,6 @@ def annotate_abstract(abstract_path, gold_annotation_path):
         ann_start = part_list[ann_index][0]
         ann_end = part_list[ann_index][1]
     in_phrase = False
-    sentence_start_ind = [0]
 
     for word_ind in range(len(word_list)):
         word = word_list[word_ind]
@@ -73,44 +71,33 @@ def annotate_abstract(abstract_path, gold_annotation_path):
                     ann_start = part_list[ann_index][0]
                     ann_end = part_list[ann_index][1]
                 in_phrase = False
-        if word[-1] == ".":
-            sentence_start_ind.append(word_ind+1)
 
     # word_list == tag_list 
     
     # writing .ann and .txt files 
-    out_abs_path = abstract_path[0:-4] + '_input.txt'
-    out_ann_path = abstract_path[0:-4] + '_input_tags.ann'
+    out_ann_path = abstract_path[0:-4] + '_tags.ann'
     
-    abs_out_str = ''
-    ann_out_str = ''
     
-    for i in range(len(sentence_start_ind)-1):  
-        sentence = ' '.join(word_list[sentence_start_ind[i]:sentence_start_ind[i+1]])
-        tag_sentence = ' '.join(tag_list[sentence_start_ind[i]:sentence_start_ind[i+1]])
-
-        abs_out_str = abs_out_str + sentence + '\n'
-        ann_out_str = ann_out_str + tag_sentence + '\n'
+    tag_sentence = ' '.join(tag_list)
+#     print tag_sentence
     
-    abs_f = open(out_abs_path, 'w')
     ann_f = open(out_ann_path, 'w')
+#     print out_ann_path
     
-    abs_f.write(abs_out_str);
-    ann_f.write(ann_out_str);
+    ann_f.write(tag_sentence);
     
-    abs_f.close();
     ann_f.close();
     
 
 
-# In[44]:
+# In[19]:
 
-abs_path = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/20957980.txt'
-ann_path = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/20957980_gold.ann'
+abs_path = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/20957980_tokens.txt'
+ann_path = 'PICO-annotations/batch5k/0074f5e102cf4409ac07f6209dd30144/20957980_gold_2.ann'
 annotate_abstract(abs_path, ann_path)
 
 
-# In[45]:
+# In[20]:
 
 directory = 'PICO-annotations/batch5k'
 
@@ -125,43 +112,30 @@ for subdir in os.listdir(directory):
     
     # For each abstract in subdirectory
     for abstract in os.listdir(subdir_path):
-        if (abstract.endswith('.txt')) and not (abstract.endswith('input.txt')):
+        if (abstract.endswith('tokens.txt')):
             abstract_path = subdir_path + '/' + abstract; 
             # print abstract_path
-            ann_path = abstract_path[0:-4] + '_gold.ann'
+            ann_path = abstract_path[0:-10] + 'gold_2.ann'
             annotate_abstract(abstract_path, ann_path)
 
 
-# In[46]:
+# In[35]:
 
 # reads a single input.txt file (and optional) input_tags.ann
 # param: separate_sentances. True [[s1w1 s1w2 s1w3] [s2w1 s2w2]] False [s1w1 s1w2 s1w3 s2w1 s2w2]
 # output [text_array, tag_array]
-def read_file(abstract_path, gold_annotation_path=None, separate_sentances=False):    
+def read_file(abstract_path, tag_path=None):    
     abstract_file = open(abstract_path, 'r');
-    file_text = abstract_file.readlines();
-    file_text = np.array([line.strip() for line in file_text]) # array of sentence strings
-    text_words_separated = np.array([line.split() for line in file_text])
-    
-    if separate_sentances: 
-        text_array = text_words_separated
-    else:
-        text_array = []
-        [text_array.extend(x) for x in text_words_separated]
+    file_text = abstract_file.read();    
+    text_array = file_text.split()
     abstract_file.close()
 
     # if gold_annotation exists
-    if gold_annotation_path:
-        tag_file = open(gold_annotation_path);
-        tags = tag_file.readlines();
-        tags = np.array([line.strip() for line in tags]) # sentences
-        tags_separated = np.array([line.split() for line in tags]) # by tag 
-        
-        if separate_sentances:
-            tag_array = tags_separated
-        else:
-            tag_array = []
-            [tag_array.extend(x) for x in tags_separated]
+    tag_array = []
+    if tag_path:
+        tag_file = open(tag_path);
+        tags = tag_file.read()
+        tag_array = tags.split()
         tag_file.close()
     
     return [text_array, tag_array]
@@ -171,14 +145,14 @@ def read_file(abstract_path, gold_annotation_path=None, separate_sentances=False
 # In[64]:
 
 abs_path = 'PICO-annotations/batch5k/ff5877cef90c40c6b3a587d71f7613d5/11229858_input.txt'
-ann_path = 'PICO-annotations/batch5k/ff5877cef90c40c6b3a587d71f7613d5/11229858_input_tags.ann'
+ann_path = 'PICO-annotations/batch5k/f f5877cef90c40c6b3a587d71f7613d5/11229858_input_tags.ann'
 [abs_array, tag_array] = read_file(abs_path, ann_path)
 
 
 # In[65]:
 
 # takes .ann and .txt files
-def get_all_data(data_directory='PICO-annotations/batch5k'):
+def get_all_data_OLD(data_directory='PICO-annotations/batch5k'):
     abstract_array = []
     tag_array = []
     
@@ -208,9 +182,49 @@ def get_all_data(data_directory='PICO-annotations/batch5k'):
     return [abstract_array, tag_array]
 
 
+# In[ ]:
+
+
+
+
 # In[66]:
 
 # [word_array, tag_array] = get_all_data();
+
+
+# In[36]:
+
+ABSTRACT_TOKENS_PATH_END = '_tokens.txt'
+ABSTRACT_TAGS_PATH_END = '_tokens_tags.ann'
+
+
+# In[33]:
+
+def get_all_data_train(train_abstract_list='PICO-annotations/train_abstracts.txt'):
+    train_abs_list = open(train_abstract_list, 'r')
+    abstract_list = train_abs_list.readlines()
+    abstract_list = [x.strip() for x in abstract_list]
+    
+    word_array = []
+    tag_array = []
+    
+    for abstract_path in abstract_list:
+        abstract_token_path = abstract_path[:-4] + ABSTRACT_TOKENS_PATH_END
+        tag_path = abstract_path[:-4] + ABSTRACT_TAGS_PATH_END
+        [curr_word_array, curr_tag_array] = read_file(abstract_token_path, tag_path)
+        if not(len(curr_word_array) == len(curr_tag_array)):
+            raise ValueError('For this file, len of abstract words and tags did not match.', abstract_path)
+        word_array.extend(curr_word_array)
+        tag_array.extend(curr_tag_array)
+    if not(len(word_array) == len(tag_array)):
+        raise ValueError('Overall, len of abstract words and tags did not match.', abstract_path)
+
+
+
+
+# In[34]:
+
+get_all_data_train();
 
 
 # In[ ]:
