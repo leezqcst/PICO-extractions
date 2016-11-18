45e29a91c854f4f7d83344f32df3fd451f4e472b
Tiffany Wong <tcwong@mit.edu>
2016-11-17 19:46:15 -0500

    got cnn to run, but accuracy = 0. Checking into accuracy function

--- TF_cnn.py
+++ TF_cnn.py
@@ -1,14 +1,18 @@
 
 # coding: utf-8
 
-# In[8]:
+# In[1]:
 
 import numpy as np
 import tensorflow as tf
 from preprocess_data import get_all_data_train
 from TF_preprocess_data import get_1_hot_abstract_encodings
-
-session = tf.InteractiveSession()
+from text_cnn import TextCNN
+import datetime
+# import data_helpers
+import time
+import os
+from tensorflow.contrib import learn
 
 # dennybritz's Sentance classification using cnn
 # https://github.com/dennybritz/cnn-text-classification-tf
@@ -18,12 +22,10 @@ session = tf.InteractiveSession()
 
 # ## Parameters
 
-# In[9]:
+# In[2]:
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
-tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the positive data.")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
@@ -35,7 +37,7 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 200, "Number of training epochs (default: 200)")
-tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
+# tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
@@ -51,15 +53,182 @@ print("")
 
 # ## Data Preperation
 
-# In[7]:
+# In[15]:
 
 word_array, tag_array = get_all_data_train()
 X, Y = get_1_hot_abstract_encodings(word_array, tag_array)
 
 
+
+# In[14]:
+
+print Y[0]
+
+
+# In[17]:
+
+# max_document_length = len(X[0])
+# vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
+
+# # Randomly shuffle data
+# np.random.seed(10)
+# shuffle_indices = np.random.permutation(np.arange(len(Y)))
+# x_shuffled = X[shuffle_indices]
+# y_shuffled = Y[shuffle_indices]
+
+# # Split train/test set
+# # TODO: This is very crude, should use cross-validation
+# dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(Y)))
+# x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
+# y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
+# print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
+# print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+
+
+# In[8]:
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data at each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index:end_index]
+
+
+# In[7]:
+
+max([max(sub_X) for sub_X in X]) + 1
+
+
 # ## Training
 
-# In[ ]:
+# In[12]:
+
+with tf.Graph().as_default():
+    session_conf = tf.ConfigProto(
+      allow_soft_placement=FLAGS.allow_soft_placement,
+      log_device_placement=FLAGS.log_device_placement)
+    sess = tf.Session(config=session_conf)
+    with sess.as_default():
+        cnn = TextCNN(
+            sequence_length=X.shape[1],
+            num_classes=Y.shape[1],
+            vocab_size=max([max(sub_X) for sub_X in X]) + 1, # TODO: get vocab size another way
+            embedding_size=FLAGS.embedding_dim,
+            filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
+            num_filters=FLAGS.num_filters,
+            l2_reg_lambda=FLAGS.l2_reg_lambda)
+
+        # Define Training procedure
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        optimizer = tf.train.AdamOptimizer(1e-3)
+        grads_and_vars = optimizer.compute_gradients(cnn.loss)
+        train_op = optimizer.apply_gradients(grads_and_vars, global_step=global_step)
+
+        # Keep track of gradient values and sparsity (optional)
+        grad_summaries = []
+        for g, v in grads_and_vars:
+            if g is not None:
+                grad_hist_summary = tf.histogram_summary("{}/grad/hist".format(v.name), g)
+                sparsity_summary = tf.scalar_summary("{}/grad/sparsity".format(v.name), tf.nn.zero_fraction(g))
+                grad_summaries.append(grad_hist_summary)
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.merge_summary(grad_summaries)
+
+        # Output directory for models and summaries
+        timestamp = str(int(time.time()))
+        out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+        print("Writing to {}\n".format(out_dir))
+
+        # Summaries for loss and accuracy
+        loss_summary = tf.scalar_summary("loss", cnn.loss)
+        acc_summary = tf.scalar_summary("accuracy", cnn.accuracy)
+
+        # Train Summaries
+        train_summary_op = tf.merge_summary([loss_summary, acc_summary, grad_summaries_merged])
+        train_summary_dir = os.path.join(out_dir, "summaries", "train")
+        train_summary_writer = tf.train.SummaryWriter(train_summary_dir, sess.graph)
+
+        # Dev summaries
+        dev_summary_op = tf.merge_summary([loss_summary, acc_summary])
+        dev_summary_dir = os.path.join(out_dir, "summaries", "dev")
+        dev_summary_writer = tf.train.SummaryWriter(dev_summary_dir, sess.graph)
+
+        # Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+        checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+        checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.all_variables())
+
+#         Write vocabulary
+#         vocab_processor.save(os.path.join(out_dir, "vocab"))
+
+        # Initialize all variables
+        sess.run(tf.initialize_all_variables())
+
+        def train_step(x_batch, y_batch):
+            """
+            A single training step
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
+            }
+            _, step, summaries, loss, accuracy = sess.run(
+                [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy],feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            train_summary_writer.add_summary(summaries, step)
+
+        def dev_step(x_batch, y_batch, writer=None):
+            """
+            Evaluates model on a dev set
+            """
+            feed_dict = {
+              cnn.input_x: x_batch,
+              cnn.input_y: y_batch,
+              cnn.dropout_keep_prob: 1.0
+            }
+            step, summaries, loss, accuracy = sess.run(
+                [global_step, dev_summary_op, cnn.loss, cnn.accuracy],
+                feed_dict)
+            time_str = datetime.datetime.now().isoformat()
+            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+            if writer:
+                writer.add_summary(summaries, step)
+
+        # Generate batches
+        batches = batch_iter(
+            list(zip(X, Y)), FLAGS.batch_size, FLAGS.num_epochs)
+        # Training loop. For each batch...
+        for batch in batches:
+            x_batch, y_batch = zip(*batch)
+            train_step(x_batch, y_batch)
+            current_step = tf.train.global_step(sess, global_step)
+#             if current_step % FLAGS.evaluate_every == 0:
+#                 print("\nEvaluation:")
+#                 dev_step(x_dev, y_dev, writer=dev_summary_writer) # from x_dev 
+#                 print("")
+            if current_step % FLAGS.checkpoint_every == 0:
+                path = saver.save(sess, checkpoint_prefix, global_step=current_step)
+                print("Saved model checkpoint to {}\n".format(path))
+
+
+# In[3]:
 
 
 
@@ -72,64 +241,6 @@ X, Y = get_1_hot_abstract_encodings(word_array, tag_array)
 # In[ ]:
 
 
-
-
-# In[ ]:
-
-
-
-
-# In[5]:
-
-data_placeholder = tf.placeholder(tf.float32, name='data_placeholder')
-labels_placeholder = tf.placeholder(tf.float32, name='labels_placeholder')
-
-
-# In[6]:
-
-feed_dict_train = {data_placeholder: batch_data, labels_placeholder : batch_labels, keep_prob_placeholder: keep_prob_rate}
-_, loss_value_train, predictions_value_train, accuracy_value_train = session.run(
-      [optimizer, loss, prediction, accuracy], feed_dict=feed_dict_train)
-
-
-# In[ ]:
-
-
-# After then, we can use :
-# input = tf.placeholder(tf.float32) #not intialized and contains no data 
-# classifier = ...
-# print(classifier.eval(feed_dict={input: my_python_preprocessing_fn()}))
-
-##### More to another file #####
-
-def placeholder_inputs(batch_size):
-    images_placeholder = tf.placeholder(tf.float32, shape=(batch_size,
-                                                         mnist.IMAGE_PIXELS))
-    labels_placeholder = tf.placeholder(tf.int32, shape=(batch_size))
-    return images_placeholder, labels_placeholder
-
-
-def fill_feed_dict(data_set, images_pl, labels_pl):
-    images_feed, labels_feed = data_set.next_batch(FLAGS.batch_size,
-                                                 FLAGS.fake_data)
-    feed_dict = {
-      images_pl: images_feed,
-      labels_pl: labels_feed,
-    }
-    
-    return feed_dict
-
-# how to we write this dataset? 
-
-# placeholders, which allow you to manually pass in numpy arrays of data.
-with tf.Session():
-  input = tf.placeholder(tf.float32) #not intialized and contains no data 
-  classifier = ...
-  print(classifier.eval(feed_dict={input: my_python_preprocessing_fn()}))
-
-x = tf.placeholder(tf.float32, shape=[2, 2])
-y = tf.constant([[1.0, 1.0], [0.0, 1.0]])
-z = tf.matmul(x, y)
 
 
 # In[ ]:
