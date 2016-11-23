4d5f951e5a58ec803a0f7958a56c4be3030b5960
Nat Sothanaphan <natsothanaphan@gmail.com>
2016-11-23 16:34:16 -0500

    Update crf_suppport.ipynb to have functions that take a look at predicted intervals vs. gold intervals

--- crf_support.py
+++ crf_support.py
@@ -6,6 +6,11 @@
 
 # In[ ]:
 
+from collections import defaultdict
+
+
+# In[ ]:
+
 def get_all_data(data_set, tag):
     switcher = {
         'train': 'PICO-annotations/train_abstracts.txt',
@@ -45,4 +50,104 @@ def get_all_data(data_set, tag):
         raise ValueError('Overall, len of abstract words and tags did not match.')
     
     return tokens_array, tags_array
+
+
+# In[ ]:
+
+def compare_tags(pred_tags_list, gold_tags_list, eval_tag):
+    count_pred = defaultdict(int)
+    length_pred = defaultdict(int)
+    count_gold = defaultdict(int)
+    length_gold = defaultdict(int)
+        
+    for i in range(len(pred_tags_list)):
+        pred_tags = pred_tags_list[i]
+        gold_tags = gold_tags_list[i]
+
+        def to_intervals(tags):
+            intervals = []
+            curr_start = None
+
+            for j in range(len(tags)+1):
+                if j == len(tags):
+                    if curr_start != None:
+                        intervals.append((curr_start, j))
+                        curr_start = None
+                elif tags[j] == eval_tag:
+                    if curr_start == None:
+                        curr_start = j
+                else:
+                    if curr_start != None:
+                        intervals.append((curr_start, j))
+                        curr_start = None
+
+            return intervals
+    
+        pred_intervals = to_intervals(pred_tags)
+        gold_intervals = to_intervals(gold_tags)
+        
+        def evaluate_intervals(source_intervals, target_intervals, count_dict, length_dict):
+            def relationship(interval_1, interval_2):
+                a, b = interval_1
+                c, d = interval_2
+
+                if a == c and b == d:
+                    return 'Identical'
+                elif b <= c or d <= a:
+                    return 'Non-overlapping'
+                elif c <= a and b <= d:
+                    return 'Subinterval'
+                elif a <= c and d <= b:
+                    return 'Superinterval'
+                else:
+                    return 'Overlapping'
+
+            def one_to_many_relationship(interval, intervals):
+                encountered = set()
+                for target_interval in intervals:
+                    relation = relationship(interval, target_interval)
+                    encountered.add(relation)
+
+                for relation in ['Identical', 'Subinterval', 'Superinterval', 'Overlapping']:
+                    if relation in encountered:
+                        return relation
+
+                return 'Non-overlapping'
+            
+            for interval in source_intervals:
+                
+                relation = one_to_many_relationship(interval, target_intervals)
+                
+                a, b = interval
+                
+                count_dict[relation] += 1
+                length_dict[relation] += b-a
+            
+            return
+        
+        evaluate_intervals(pred_intervals, gold_intervals, count_pred, length_pred)
+        evaluate_intervals(gold_intervals, pred_intervals, count_gold, length_gold)
+    
+    def print_result(count_pred, length_pred, count_gold, length_gold):
+        types = ['Identical', 'Subinterval', 'Superinterval', 'Overlapping', 'Non-overlapping']
+        
+        switcher = [
+            (count_pred, 'predicted', 'intervals'),
+            (length_pred, 'predicted', 'tokens'),
+            (count_gold, 'gold', 'intervals'),
+            (length_gold, 'gold', 'tokens')
+        ]
+        
+        for dictionary, p_or_g, i_or_t in switcher:
+            total = sum(dictionary.values())
+            
+            print 'There are {} {} {}:'.format(total, p_or_g, i_or_t)
+            for interval_type in types:
+                print 'Number of type {}: {}'.format(interval_type+' '*(15-len(interval_type)),                                                     dictionary[interval_type])
+            print ''
+            
+        return
+    
+    print_result(count_pred, length_pred, count_gold, length_gold)
+    return
 
