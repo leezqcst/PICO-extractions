8c8a624bd3a1fbeaaa5623e293b8c7683c023d98
Tiffany Wong <tcwong@mit.edu>
2016-12-05 23:24:29 -0500

    added dev testing

--- TF_cnn_by_word.py
+++ TF_cnn_by_word.py
@@ -6,6 +6,7 @@
 import numpy as np
 import tensorflow as tf
 from preprocess_data import get_all_data_train
+from preprocess_data import get_all_data_dev
 from TF_preprocess_data import get_1_hot_sentence_encodings
 from text_cnn_by_word import TextCNN
 import datetime
@@ -20,12 +21,13 @@ from tensorflow.contrib import learn
 # https://github.com/nicholaslocascio/tensorflow-nlp-tutorial/blob/master/sentiment-analysis/Sentiment-RNN.ipynb
 
 
-# ## Parameters
+# # Parameters
 
 # In[2]:
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
+tf.flags.DEFINE_integer("word_padding_size", 3, "Number of words for padding front and back")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 64, "Dimensionality of character embedding (default: 128)")
@@ -37,12 +39,12 @@ tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 1, "Number of training epochs (default: 200)")
-# tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps (default: 100)")
-tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps (default: 100)")
+tf.flags.DEFINE_integer("evaluate_every", 1000, "Evaluate model on dev set after this many steps (default: 100)")
+tf.flags.DEFINE_integer("checkpoint_every", 500, "Save model after this many steps (default: 100)")
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
 tf.flags.DEFINE_boolean("log_device_placement", False, "Log placement of ops on devices")
-tf.flags.DEFINE_integer("eval_batches", 1, "Number of batches output to use when calculating precision, recall and f1")
+tf.flags.DEFINE_integer("eval_batches", 250, "Number of batches output to use when calculating precision, recall and f1")
 
 FLAGS = tf.flags.FLAGS
 FLAGS._parse_flags()
@@ -52,97 +54,105 @@ for attr, value in sorted(FLAGS.__flags.items()):
 print("")
 
 
-# ## Data Preperation
+# # Data Preparation
 
 # In[3]:
 
-# split into train and dev 
-word_array, tag_array = get_all_data_train(sentences=False)
+# n words to pad on each side of the word 
+def get_train_data(n):
+    # split into train and dev 
+    word_array, tag_array = get_all_data_train(sentences=False)
+    x_n_padded, y = process_data_into_chunks(word_array, tag_array, n)
+    
+    # Build vocabulary
+    document_length = 2*n+1
+    vocab_processor = learn.preprocessing.VocabularyProcessor(document_length)
+    
+    x = np.array(list(vocab_processor.fit_transform(x_n_padded)))
+    
+    return x, y, vocab_processor
 
 
 # In[4]:
 
-# pad single words with n words on either side 
-n = 3
-x_n = []
-for abstract in word_array:
-    # pad abstract with * 
-    padding = ["*"] * n
-    padded_abstract = padding
-    padded_abstract.extend(abstract)
-    padded_abstract.extend(padding)
-    # for all words (excluding padding)
-    for i in range(n, len(abstract)+n):
-        x_n.append(padded_abstract[i-n:i+n+1])
-
-y_binary = [y for x in tag_array for y in x] # flatten tag array
-y = np.array([[1,0] if tag == 'P' else [0,1] for tag in y_binary ])
+def get_dev_data(n, vocab_processor):
+    word_array, tag_array = get_all_data_dev(sentences=False)
+    x_n_padded, y = process_data_into_chunks(word_array, tag_array, n)
+    
+    x = np.array(list(vocab_processor.transform(x_n_padded)))
+    
+    return x,y  
 
 
 # In[5]:
 
-# print x_n[150:200]
-# print y[150:200]
+def process_data_into_chunks(word_array, tag_array, n):
+    x_n = []
+    for abstract in word_array:
+        # pad abstract with * 
+        padding = ["*"] * n
+        padded_abstract = padding
+        padded_abstract.extend(abstract)
+        padded_abstract.extend(padding)
+        # for all words (excluding padding)
+        for i in range(n, len(abstract)+n):
+            x_n.append(padded_abstract[i-n:i+n+1])
+            
+    x_n_padded = [' '.join(word) for word in x_n]
+
+    y_binary = [y for x in tag_array for y in x] # flatten tag array
+    y = np.array([[1,0] if tag == 'P' else [0,1] for tag in y_binary ])
+    
+    return x_n_padded, y
 
 
 # In[6]:
 
-# Build vocabulary
-document_length = 2*n+1
-vocab_processor = learn.preprocessing.VocabularyProcessor(document_length)
-n_array = [' '.join(word) for word in x_n]
-x = np.array(list(vocab_processor.fit_transform(n_array)))
+x_train, y_train, vocab_processor = get_train_data(FLAGS.word_padding_size)
 
 
 # In[7]:
 
-# print type(x)
-# print type(y)
-# print x.shape
-# print y.shape
-# print x[0]
-# print x_n[0]
-# print y[0]
-
-# print x[1]
-# print x_n[1]
-# print x[2]
-# print x_n[2]
-# print x[3]
-# print x_n[3]
-# print x[4]
-# print x_n[4]
-# print x[5]
-# print x_n[5]
-# print x[6]
-# print x_n[6]
-# print x[7]
-# print x_n[7]
-# print x[11]
-# print x_n[11]
+x_dev, y_dev = get_dev_data(FLAGS.word_padding_size, vocab_processor)
 
 
 # In[8]:
 
-# max_document_length = len(X[0])
-# vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
-
-# # # Randomly shuffle data
-# np.random.seed(10)
-# shuffle_indices = np.random.permutation(np.arange(len(Y)))
-# x_shuffled = X[shuffle_indices]
-# y_shuffled = Y[shuffle_indices]
-
-# # Split train/test set
-# # TODO: This is very crude, should use cross-validation
-# dev_sample_index = -1 * int(FLAGS.dev_sample_percentage * float(len(Y)))
-# x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
-# y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
-# print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
-# print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
+# split into train and dev 
+# word_array, tag_array = get_all_data_train(sentences=False)
 
 
 # In[9]:
+
+# # pad single words with n words on either side 
+# n = 3
+# x_n = []
+# for abstract in word_array:
+#     # pad abstract with * 
+#     padding = ["*"] * n
+#     padded_abstract = padding
+#     padded_abstract.extend(abstract)
+#     padded_abstract.extend(padding)
+#     # for all words (excluding padding)
+#     for i in range(n, len(abstract)+n):
+#         x_n.append(padded_abstract[i-n:i+n+1])
+
+# y_binary = [y for x in tag_array for y in x] # flatten tag array
+# y = np.array([[1,0] if tag == 'P' else [0,1] for tag in y_binary ])
+
+
+# In[10]:
+
+# # Build vocabulary
+# document_length = 2*n+1
+# vocab_processor = learn.preprocessing.VocabularyProcessor(document_length)
+# n_array = [' '.join(word) for word in x_n]
+# x = np.array(list(vocab_processor.fit_transform(n_array)))
+
+
+# # Helper Functions
+
+# In[11]:
 
 # copied unchanged function
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
@@ -165,47 +175,46 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
             yield shuffled_data[start_index:end_index]
 
 
-# In[10]:
-
-# len(vocab_processor.vocabulary_)
-
-
-# ## Helper Functions
-
-# In[17]:
+# In[12]:
 
 def get_eval_counts(truth, predictions):
-    gold_tags = truth
-    pred_tags = predictions
 
-    unique, counts = np.unique(pred_tags, return_counts=True)
-    pred_tag_dict = dict(zip(unique, counts))
-    p_tokens_extracted = pred_tag_dict.get(0,0)
+    p_tokens_extracted = len(predictions) - sum(predictions)
+    p_true_tokens = len(truth) - sum(truth)
+    p_tokens_correct = sum([1 for i, j in zip(truth, predictions) if (i == 0 and j == 0)])
     
-    intersection = (gold_tags == pred_tags)
-    p_tokens = (gold_tags == 0) 
-    p_tokens_correct = (((intersection*1)+(p_tokens*1)))== 2
-    
-    unique, counts = np.unique(p_tokens_correct, return_counts=True)
-    p_tokens_correct_tag_dict = dict(zip(unique, counts))
-    p_tokens_correct = pred_tag_dict.get(True, 0)
-        
-    unique, counts = np.unique(gold_tags, return_counts=True)
-    gold_tag_dict = dict(zip(unique, counts))
-    p_true_tokens = gold_tag_dict.get(0, 0)
-
-    print (p_tokens_extracted, p_tokens_correct, p_true_tokens)
+#     print (p_tokens_extracted, p_tokens_correct, p_true_tokens)
     return (p_tokens_extracted, p_tokens_correct, p_true_tokens)
 
 
 # In[ ]:
 
+def calculate_precision_recall_f1(p_tokens_extracted, p_tokens_correct, p_true_tokens):
+    if (p_tokens_extracted == 0):
+        if (p_tokens_correct == 0):
+            precision = 1
+        else:
+            precision = 0
+    else:
+        precision = float(p_tokens_correct)/float(p_tokens_extracted)
 
+    if (p_true_tokens == 0):
+        if (p_tokens_correct == 0):
+            recall = 1
+        else:
+            recall = 0
+    else:
+        recall = float(p_tokens_correct)/float(p_true_tokens)
+    if ((precision + recall) == 0):
+        f1 = 0
+    else:
+        f1 = (2*precision*recall)/(precision+recall)
+    return (precision, recall, f1)
 
 
 # ## Training
 
-# In[18]:
+# In[ ]:
 
 with tf.Graph().as_default():
     session_conf = tf.ConfigProto(
@@ -214,8 +223,8 @@ with tf.Graph().as_default():
     sess = tf.Session(config=session_conf)
     with sess.as_default():
         cnn = TextCNN(
-            sequence_length=x.shape[1],
-            num_classes=y.shape[1],
+            sequence_length=x_train.shape[1],
+            num_classes=y_train.shape[1],
             vocab_size=len(vocab_processor.vocabulary_),
             embedding_size=FLAGS.embedding_dim,
             filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),
@@ -280,7 +289,7 @@ with tf.Graph().as_default():
               cnn.dropout_keep_prob: FLAGS.dropout_keep_prob
             }
             # TODO: uncomment and add scores
-            _, step, summaries, loss, accuracy, input_y, predictions, input_x, scores, temp = sess.run(
+            _, step, summaries, loss, accuracy, input_y, predictions, input_x, scores, truth = sess.run(
                 [train_op, global_step, train_summary_op, cnn.loss, cnn.accuracy, cnn.input_y, cnn.predictions, cnn.input_x, cnn.scores, cnn.temp],feed_dict)
             # remove below afterwards  
 #             _, step, summaries, loss, accuracy = sess.run(
@@ -291,7 +300,9 @@ with tf.Graph().as_default():
             # TODO UNCOMMENT BELOW
 #             print("{}: step {}, loss {:g}, pre {:g}, rec {:g}, f1 {:g}".format(time_str, step, loss, precision, recall, f1))
             #temp
-            print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
+    
+            if (int(step)%FLAGS.eval_batches == 0):
+                print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
 
             train_summary_writer.add_summary(summaries, step)
             
@@ -318,7 +329,7 @@ with tf.Graph().as_default():
 #             print temp
 #             print " "
             
-            return get_eval_counts(temp, predictions)
+            return get_eval_counts(truth, predictions)
             
             
 
@@ -331,24 +342,29 @@ with tf.Graph().as_default():
               cnn.input_y: y_batch,
               cnn.dropout_keep_prob: 1.0
             }
-            step, summaries, loss, accuracy, scores, predictions = sess.run(
-    
-                [global_step, dev_summary_op, cnn.loss, cnn.accuracy, cnn.scores, cnn.predictions],
+            
+            
+            step, summaries, loss, accuracy, scores, predictions, truth = sess.run(
+                [global_step, dev_summary_op, cnn.loss, cnn.accuracy, cnn.scores, cnn.predictions, cnn.temp],
                 feed_dict)
             time_str = datetime.datetime.now().isoformat()
             print("{}: step {}, loss {:g}, acc {:g}".format(time_str, step, loss, accuracy))
             if writer:
                 writer.add_summary(summaries, step)
+            return get_eval_counts(truth, predictions)
+
 
         # Generate batches
-        batches = batch_iter(
-            list(zip(x, y)), FLAGS.batch_size, FLAGS.num_epochs)
+        batches_train = batch_iter(
+            list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
         # Training loop. For each batch...
         batches_since_last_eval_calc = 0
         p_tokens_extracted_tot = 0
         p_tokens_correct_tot = 0
         p_true_tokens_tot = 0
-        for batch in batches:
+        
+        
+        for batch in batches_train:
             x_batch, y_batch = zip(*batch)
             (p_tokens_extracted, p_tokens_correct, p_true_tokens) = train_step(x_batch, y_batch)
             p_tokens_extracted_tot += p_tokens_extracted
@@ -357,34 +373,22 @@ with tf.Graph().as_default():
             
             batches_since_last_eval_calc += 1
             if batches_since_last_eval_calc == FLAGS.eval_batches:
-                if (p_tokens_extracted_tot == 0):
-                    if (p_tokens_correct_tot == 0):
-                        precision = 1
-                    else:
-                        precision = 0
-                else:
-                    precision = float(p_tokens_correct_tot)/float(p_tokens_extracted_tot)
-                
-                if (p_true_tokens_tot == 0):
-                    if (p_tokens_correct_tot == 0):
-                        recall = 1
-                    else:
-                        recall = 0
-                else:
-                    recall = float(p_tokens_correct_tot)/float(p_true_tokens_tot)
-                f1 = (2*precision*recall)/(precision+recall)
+                (precision, recall, f1) = calculate_precision_recall_f1(p_tokens_extracted_tot, p_tokens_correct_tot, p_true_tokens_tot)
                 print("correct: {:g}, extracted: {:g}, true: {:g}".format(p_tokens_correct_tot, p_tokens_extracted_tot, p_true_tokens_tot))
-                print("Precision: {:g}, recall: {:g}, f1: {:g}".format(precision, recall, f1))
+                print("TRAIN  Precision: {:g}, recall: {:g}, f1: {:g}".format(precision, recall, f1))
+                print " "
                 p_tokens_extracted_tot = 0
                 p_tokens_correct_tot = 0
                 p_true_tokens_tot = 0
                 batches_since_last_eval_calc = 0
             
             current_step = tf.train.global_step(sess, global_step)
-#             if current_step % FLAGS.evaluate_every == 0:
-#                 print("\nEvaluation:")
-#                 dev_step(x_dev, y_dev, writer=dev_summary_writer) # from x_dev 
-#                 print("")
+            if current_step % FLAGS.evaluate_every == 0:
+                print("\nEvaluation:")
+                (p_tokens_extracted, p_tokens_correct, p_true_tokens) = dev_step(x_dev, y_dev, writer=dev_summary_writer) # from x_dev 
+                (precision, recall, f1) = calculate_precision_recall_f1(p_tokens_extracted, p_tokens_correct, p_true_tokens)
+                print("DEV  Precision: {:g}, recall: {:g}, f1: {:g}".format(precision, recall, f1))
+                print("")
             if current_step % FLAGS.checkpoint_every == 0:
                 path = saver.save(sess, checkpoint_prefix, global_step=current_step)
                 print("Saved model checkpoint to {}\n".format(path))
