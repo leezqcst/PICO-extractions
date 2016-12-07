0d0f86275d62e1e4981220aa24b00334b4c7a893
Tiffany Wong <tcwong@mit.edu>
2016-12-06 21:48:50 -0500

    updated params in cnn

--- TF_cnn_by_word.py
+++ TF_cnn_by_word.py
@@ -27,14 +27,14 @@ from tensorflow.contrib import learn
 
 # Data loading params
 tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training data to use for validation")
-tf.flags.DEFINE_integer("word_padding_size", 3, "Number of words for padding front and back")
+tf.flags.DEFINE_integer("word_padding_size", 6, "Number of words for padding front and back")
 
 # Model Hyperparameters
 tf.flags.DEFINE_integer("embedding_dim", 64, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 64, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
-tf.flags.DEFINE_float("l2_reg_lambda", 0.0, "L2 regularizaion lambda (default: 0.0)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0.001, "L2 regularizaion lambda (default: 0.0)")
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
